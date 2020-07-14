548dfa7d669aa1361cddc602d0d5fc0ba415e87c
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2020-07-14 19:24:26 +0200

    Update zzupdate.sh

--- zzupdate.sh
+++ zzupdate.sh
@@ -117,7 +117,7 @@ ZZSCRIPT_DIRS=($(find $INSTALL_DIR_PARENT -maxdepth 1 -type d))
 
 for ZZSCRIPT_DIR in "${ZZSCRIPT_DIRS[@]}"; do
 
-	if [ "${ZZSCRIPT_DIR}/" != "$SCRIPT_DIR} ] &&  [ -d "${ZZSCRIPT_DIR}.git" ]; then
+	if [ "${ZZSCRIPT_DIR}/" != "$SCRIPT_DIR" ] &&  [ -d "${ZZSCRIPT_DIR}.git" ]; then
 	
 		printTitle "Update ${ZZSCRIPT_DIR}..."
 		git -C "$ZZSCRIPT_DIR" pull
