9634fe2519525ed355abf8e052b43801798e581b
Zane <info@zane.it>
2020-12-17 00:13:57 +0100

    - replace tab with spaces - add -j4 to Nimbus compilation (4 threads)

--- zzupdate.sh
+++ zzupdate.sh
@@ -41,56 +41,56 @@ function printTitle
 ## root check
 if ! [ $(id -u) = 0 ]; then
 
-		echo ""
-		echo "vvvvvvvvvvvvvvvvvvvv"
-		echo "Catastrophic error!!"
-		echo "^^^^^^^^^^^^^^^^^^^^"
-		echo "This script must run as root!"
+	echo ""
+    echo "vvvvvvvvvvvvvvvvvvvv"
+	echo "Catastrophic error!!"
+	echo "^^^^^^^^^^^^^^^^^^^^"
+	echo "This script must run as root!"
 
-		printTitle "How to fix it?"
-		echo "Execute the script like this:"
-		echo "sudo $SCRIPT_NAME"
+	printTitle "How to fix it?"
+	echo "Execute the script like this:"
+	echo "sudo $SCRIPT_NAME"
 
-		printTitle "The End"
-		echo $(date)
-		echo "$FRAME"
-		exit
+	printTitle "The End"
+	echo $(date)
+	echo "$FRAME"
+	exit
 fi
 
 ## Profile requested
 if [ ! -z "$1" ]; then
 
-	CONFIGFILE_PROFILE_NAME=${SCRIPT_NAME}.profile.${1}.conf
-	CONFIGFILE_PROFILE_FULLPATH_ETC=/etc/turbolab.it/$CONFIGFILE_PROFILE_NAME
-	CONFIGFILE_PROFILE_FULLPATH_DIR=${SCRIPT_DIR}$CONFIGFILE_PROFILE_NAME
+    CONFIGFILE_PROFILE_NAME=${SCRIPT_NAME}.profile.${1}.conf
+    CONFIGFILE_PROFILE_FULLPATH_ETC=/etc/turbolab.it/$CONFIGFILE_PROFILE_NAME
+    CONFIGFILE_PROFILE_FULLPATH_DIR=${SCRIPT_DIR}$CONFIGFILE_PROFILE_NAME
 
-	if [ ! -f "$CONFIGFILE_PROFILE_FULLPATH_ETC" ] && [ ! -f "$CONFIGFILE_PROFILE_FULLPATH_DIR" ]; then
+    if [ ! -f "$CONFIGFILE_PROFILE_FULLPATH_ETC" ] && [ ! -f "$CONFIGFILE_PROFILE_FULLPATH_DIR" ]; then
 
-		echo ""
-		echo "vvvvvvvvvvvvvvvvvvvv"
-		echo "Catastrophic error!!"
-		echo "^^^^^^^^^^^^^^^^^^^^"
-		echo "Profile config file(s) not found:"
-		echo "[X] $CONFIGFILE_PROFILE_FULLPATH_ETC"
-		echo "[X] $CONFIGFILE_PROFILE_FULLPATH_DIR"
+        echo ""
+        echo "vvvvvvvvvvvvvvvvvvvv"
+        echo "Catastrophic error!!"
+        echo "^^^^^^^^^^^^^^^^^^^^"
+        echo "Profile config file(s) not found:"
+        echo "[X] $CONFIGFILE_PROFILE_FULLPATH_ETC"
+        echo "[X] $CONFIGFILE_PROFILE_FULLPATH_DIR"
 
-		printTitle "How to fix it?"
-		echo "Create a config file for this profile:"
-		echo "sudo cp $CONFIGFILE_FULLPATH_DEFAULT $CONFIGFILE_PROFILE_FULLPATH_ETC && sudo nano $CONFIGFILE_PROFILE_FULLPATH_ETC && sudo chmod ugo=rw /etc/turbolab.it/*.conf"
+        printTitle "How to fix it?"
+        echo "Create a config file for this profile:"
+        echo "sudo cp $CONFIGFILE_FULLPATH_DEFAULT $CONFIGFILE_PROFILE_FULLPATH_ETC && sudo nano $CONFIGFILE_PROFILE_FULLPATH_ETC && sudo chmod ugo=rw /etc/turbolab.it/*.conf"
 
-		printTitle "The End"
-		echo $(date)
-		echo "$FRAME"
-		exit
-	fi
+        printTitle "The End"
+        echo $(date)
+        echo "$FRAME"
+        exit
+    fi
 fi
 
 
 for CONFIGFILE_FULLPATH in "$CONFIGFILE_FULLPATH_DEFAULT" "$CONFIGFILE_MYSQL_FULLPATH_ETC" "$CONFIGFILE_FULLPATH_ETC" "$CONFIGFILE_FULLPATH_DIR" "$CONFIGFILE_PROFILE_FULLPATH_ETC" "$CONFIGFILE_PROFILE_FULLPATH_DIR"
 do
-	if [ -f "$CONFIGFILE_FULLPATH" ]; then
-		source "$CONFIGFILE_FULLPATH"
-	fi
+    if [ -f "$CONFIGFILE_FULLPATH" ]; then
+        source "$CONFIGFILE_FULLPATH"
+    fi
 done
 
 
@@ -100,17 +100,17 @@ source "${SCRIPT_DIR}setup.sh"
 SCRIPT_HASH_AFTER_UPDATE=`md5sum ${SCRIPT_FULLPATH} | awk '{ print $1 }'`
 if [ "$SCRIPT_HASH" != "$SCRIPT_HASH_AFTER_UPDATE" ]; then
 
-		echo ""
-		echo "vvvvvvvvvvvvvvvvvvvvvv"
-		echo "Self-update installed!"
-		echo "^^^^^^^^^^^^^^^^^^^^^^"
-		echo "zzupdate itself has been updated!"
-		echo "Please run zzupdate again to update your system."
+    echo ""
+    echo "vvvvvvvvvvvvvvvvvvvvvv"
+    echo "Self-update installed!"
+    echo "^^^^^^^^^^^^^^^^^^^^^^"
+    echo "zzupdate itself has been updated!"
+    echo "Please run zzupdate again to update your system."
 
-		printTitle "The End"
-		echo $(date)
-		echo "$FRAME"
-		exit
+    printTitle "The End"
+    echo $(date)
+    echo "$FRAME"
+    exit
 fi
 
 INSTALL_DIR_PARENT="/usr/local/turbolab.it/"
@@ -118,21 +118,21 @@ ZZSCRIPT_DIRS=($(find $INSTALL_DIR_PARENT -maxdepth 1 -type d))
 
 for ZZSCRIPT_DIR in "${ZZSCRIPT_DIRS[@]}"; do
 
-	if [ "${ZZSCRIPT_DIR}/" != "$SCRIPT_DIR" ] && [ -d "${ZZSCRIPT_DIR}/.git" ]; then
+    if [ "${ZZSCRIPT_DIR}/" != "$SCRIPT_DIR" ] && [ -d "${ZZSCRIPT_DIR}/.git" ]; then
 
-		printTitle "Update ${ZZSCRIPT_DIR}..."
-		git -C "$ZZSCRIPT_DIR" pull --no-rebase
-	fi
+        printTitle "Update ${ZZSCRIPT_DIR}..."
+        git -C "$ZZSCRIPT_DIR" pull --no-rebase
+    fi
 done
 
 if [ "$SWITCH_PROMPT_TO_NORMAL" = "1" ]; then
 
-	printTitle "Switching to the 'normal' release channel (if 'never' or 'lts')"
-	sed -i -E 's/Prompt=(never|lts)/Prompt=normal/g' "/etc/update-manager/release-upgrades"
+    printTitle "Switching to the 'normal' release channel (if 'never' or 'lts')"
+    sed -i -E 's/Prompt=(never|lts)/Prompt=normal/g' "/etc/update-manager/release-upgrades"
 
 else
 
-	printTitle "Channel switching is disabled: using pre-existing setting"
+    printTitle "Channel switching is disabled: using pre-existing setting"
 
 fi
 
@@ -148,62 +148,62 @@ apt-get dist-upgrade -y
 
 if [ "$VERSION_UPGRADE" = "1" ] && [ "$VERSION_UPGRADE_SILENT" = "1" ]; then
 
-	printTitle "Silently upgrade to a new release, if any"
-	do-release-upgrade -f DistUpgradeViewNonInteractive
+    printTitle "Silently upgrade to a new release, if any"
+    do-release-upgrade -f DistUpgradeViewNonInteractive
 
 elif [ "$VERSION_UPGRADE" = "1" ] && [ "$VERSION_UPGRADE_SILENT" = "0" ]; then
 
-	printTitle "Interactively upgrade to a new release, if any"
-	do-release-upgrade
+    printTitle "Interactively upgrade to a new release, if any"
+    do-release-upgrade
 
 else
 
-	printTitle "Upgrade to a new release skipped (disabled in config)"
+    printTitle "Upgrade to a new release skipped (disabled in config)"
 
 fi
 
 if [ "$COMPOSER_UPGRADE" = "1" ]; then
 
-	printTitle "Self-updating Composer"
+    printTitle "Self-updating Composer"
 
-	if ! [ -x "$(command -v composer)" ]; then
-		echo "Composer is not installed"
-	else
-		composer self-update
-	fi
+    if ! [ -x "$(command -v composer)" ]; then
+        echo "Composer is not installed"
+    else
+        composer self-update
+    fi
 fi
 
 if [ "$SYMFONY_UPGRADE" = "1" ]; then
 
-	printTitle "Self-updating Symfony"
+    printTitle "Self-updating Symfony"
 
-	if ! [ -x "$(command -v symfony)" ]; then
-		echo "Symfony is not installed"
-	else
-		symfony self:update --yes
-	fi
+    if ! [ -x "$(command -v symfony)" ]; then
+        echo "Symfony is not installed"
+    else
+        symfony self:update --yes
+    fi
 fi
 
 if [ "$NIMBUS_UPGRADE" = "1" ]; then
 
-	printTitle "Updating Nimbus"
-	
-	if [ -f "/usr/local/bin/nimbus" ]; then
-	
-		cd $HOME
-		git clone https://github.com/status-im/nimbus-eth2.git
-		cd nimbus-eth2
-		make beacon_node
-		mv $HOME/nimbus-eth2/build/beacon_node /usr/local/bin/nimbus
-		cd $HOME
-		rm -rf $HOME/nimbus-eth2
-		service nimbus restart
-		nimbus --version
-		
-	else
-		
-		echo "Nimbus is not installed"
-	fi
+    printTitle "Updating Nimbus"
+    
+    if [ -f "/usr/local/bin/nimbus" ]; then
+    
+        cd $HOME
+        git clone https://github.com/status-im/nimbus-eth2.git
+        cd nimbus-eth2
+        make -j4 beacon_node
+        mv $HOME/nimbus-eth2/build/beacon_node /usr/local/bin/nimbus
+        cd $HOME
+        rm -rf $HOME/nimbus-eth2
+        service nimbus restart
+        nimbus --version
+        
+    else
+        
+        echo "Nimbus is not installed"
+    fi
 fi
 
 
@@ -217,14 +217,14 @@ printTitle "Time took"
 echo "$((($(date +%s)-$TIME_START)/60)) min."
 
 if [ "$REBOOT" = "1" ]; then
-	printTitle "Rebooting"
+    printTitle "Rebooting"
 
-	while [ $REBOOT_TIMEOUT -gt 0 ]; do
-	   echo -ne "$REBOOT_TIMEOUT\033[0K\r"
-	   sleep 1
-	   : $((REBOOT_TIMEOUT--))
-	done
-	reboot
+    while [ $REBOOT_TIMEOUT -gt 0 ]; do
+       echo -ne "$REBOOT_TIMEOUT\033[0K\r"
+       sleep 1
+       : $((REBOOT_TIMEOUT--))
+    done
+    reboot
 fi
 
 printTitle "Switching back to $INITIAL_DIR"
