ccd8dec8ad2d3c19467526dadb5793b5e63e1302
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2022-06-18 00:26:48 +0200

    🌟 MAJOR NEW VERSION! bash-fx based

--- zzupdate.sh
+++ zzupdate.sh
@@ -1,129 +1,63 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo ""
 
-## Script name
-SCRIPT_NAME=zzupdate
-
-## Title and graphics
-FRAME="O===========================================================O"
-echo "$FRAME"
-echo "    $SCRIPT_NAME - $(date)"
-echo "$FRAME"
-
-## Enviroment variables
-TIME_START="$(date +%s)"
-DOWEEK="$(date +'%u')"
-HOSTNAME="$(hostname)"
-INITIAL_DIR=$(pwd)
-
-## Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT_FULLPATH=$(readlink -f "$0")
-SCRIPT_HASH=`md5sum ${SCRIPT_FULLPATH} | awk '{ print $1 }'`
-
-## Absolute path this script is in, thus /home/user/bin
-SCRIPT_DIR=$(dirname "$SCRIPT_FULLPATH")/
-
-## Config files
-CONFIGFILE_NAME=$SCRIPT_NAME.conf
-CONFIGFILE_FULLPATH_DEFAULT=${SCRIPT_DIR}$SCRIPT_NAME.default.conf
-CONFIGFILE_FULLPATH_ETC=/etc/turbolab.it/$CONFIGFILE_NAME
-CONFIGFILE_FULLPATH_DIR=${SCRIPT_DIR}$CONFIGFILE_NAME
-
-## Title printing function
-function printTitle
-{
-  echo ""
-  echo "$1"
-  printf '%0.s-' $(seq 1 ${#1})
-  echo ""
-}
-
-## root check
-if ! [ $(id -u) = 0 ]; then
-
-  echo ""
-  echo "vvvvvvvvvvvvvvvvvvvv"
-  echo "Catastrophic error!!"
-  echo "^^^^^^^^^^^^^^^^^^^^"
-  echo "This script must run as root!"
-
-  printTitle "How to fix it?"
-  echo "Execute the script like this:"
-  echo "sudo $SCRIPT_NAME"
-
-  printTitle "The End"
-  echo $(date)
-  echo "$FRAME"
-  exit
-fi
+source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+fxHeader "🔃 zzupdate 🔃"
+rootCheck
+fxConfigLoader
 
 ## Profile requested
 if [ ! -z "$1" ]; then
+
+  fxTitle "📋 Profile requested..."
 
   CONFIGFILE_PROFILE_NAME=${SCRIPT_NAME}.profile.${1}.conf
   CONFIGFILE_PROFILE_FULLPATH_ETC=/etc/turbolab.it/$CONFIGFILE_PROFILE_NAME
   CONFIGFILE_PROFILE_FULLPATH_DIR=${SCRIPT_DIR}$CONFIGFILE_PROFILE_NAME
 
   if [ ! -f "$CONFIGFILE_PROFILE_FULLPATH_ETC" ] && [ ! -f "$CONFIGFILE_PROFILE_FULLPATH_DIR" ]; then
-
+  
+    fxCatastrophicError "Profile config file(s) not found"
+    echo "🕳️ $CONFIGFILE_PROFILE_FULLPATH_ETC"
+    echo "🕳️ $CONFIGFILE_PROFILE_FULLPATH_DIR"
     echo ""
-    echo "vvvvvvvvvvvvvvvvvvvv"
-    echo "Catastrophic error!!"
-    echo "^^^^^^^^^^^^^^^^^^^^"
-    echo "Profile config file(s) not found:"
-    echo "[X] $CONFIGFILE_PROFILE_FULLPATH_ETC"
-    echo "[X] $CONFIGFILE_PROFILE_FULLPATH_DIR"
-
-    printTitle "How to fix it?"
+    
+    fxTitle "How to fix it"
     echo "Create a config file for this profile:"
-    echo "sudo cp $CONFIGFILE_FULLPATH_DEFAULT $CONFIGFILE_PROFILE_FULLPATH_ETC && sudo nano $CONFIGFILE_PROFILE_FULLPATH_ETC && sudo chmod ugo=rw /etc/turbolab.it/*.conf"
+    echo "sudo cp $CONFIGFILE_FULLPATH_DEFAULT $CONFIGFILE_PROFILE_FULLPATH_ETC && sudo nano $CONFIGFILE_PROFILE_FULLPATH_ETC && sudo chmod u=rwx,go=rx /etc/turbolab.it/zzupdate*"
 
-    printTitle "The End"
-    echo $(date)
-    echo "$FRAME"
+    fxEndFooter failure
     exit
   fi
+  
+  fxLoadConfigFromInput "$CONFIGFILE_PROFILE_FULLPATH_ETC" "$CONFIGFILE_PROFILE_FULLPATH_DIR"
+  
 fi
 
+## self-update
+HASH_BEFORE=$(fxHashFile "${SCRIPT_FULLPATH}")
 
-for CONFIGFILE_FULLPATH in "$CONFIGFILE_FULLPATH_DEFAULT" "$CONFIGFILE_MYSQL_FULLPATH_ETC" "$CONFIGFILE_FULLPATH_ETC" "$CONFIGFILE_FULLPATH_DIR" "$CONFIGFILE_PROFILE_FULLPATH_ETC" "$CONFIGFILE_PROFILE_FULLPATH_DIR"
-do
-  if [ -f "$CONFIGFILE_FULLPATH" ]; then
-    source "$CONFIGFILE_FULLPATH"
-  fi
-done
-
-
-printTitle "Self-update...."
+fxTitle "Self-update...."
 git -C "${SCRIPT_DIR}" pull --no-rebase
-source "${SCRIPT_DIR}setup.sh"
+bash "${SCRIPT_DIR}setup.sh"
 
-SCRIPT_HASH_AFTER_UPDATE=`md5sum ${SCRIPT_FULLPATH} | awk '{ print $1 }'`
-if [ "$SCRIPT_HASH" != "$SCRIPT_HASH_AFTER_UPDATE" ]; then
+HASH_AFTER=$(fxHashFile "${SCRIPT_FULLPATH}")
 
-  echo ""
-  echo "vvvvvvvvvvvvvvvvvvvvvv"
-  echo "Self-update installed!"
-  echo "^^^^^^^^^^^^^^^^^^^^^^"
-  echo "zzupdate itself has been updated!"
-  echo "Please run zzupdate again to update your system."
+fxSelfUpdateHashCheck "${HASH_BEFORE}" "${HASH_AFTER}"
 
-  printTitle "The End"
-  echo $(date)
-  echo "$FRAME"
-  exit
-fi
 
-INSTALL_DIR_PARENT="/usr/local/turbolab.it/"
+## update every script by TurboLab.it
 ZZSCRIPT_DIRS=($(find $INSTALL_DIR_PARENT -maxdepth 1 -type d))
 
 for ZZSCRIPT_DIR in "${ZZSCRIPT_DIRS[@]}"; do
 
-  if [ "${ZZSCRIPT_DIR}/" = "$SCRIPT_DIR" ] || [ ! -d "${ZZSCRIPT_DIR}/.git" ]; then
+  ZZSCRIPT_DIR_NAME=$(basename "${ZZSCRIPT_DIR}")
+  
+  if [ "${ZZSCRIPT_DIR}/" = "$SCRIPT_DIR" ] || [ ! -d "${ZZSCRIPT_DIR}/.git" ] || [ "${ZZSCRIPT_DIR_NAME}" = "bash-fx" ]; then
     continue
   fi
   
-  printTitle "***** Update ${ZZSCRIPT_DIR}... *****"
+  fxTitle "***** Update ${ZZSCRIPT_DIR}... *****"
   git -C "$ZZSCRIPT_DIR" pull --no-rebase
   
   if [ -f "${ZZSCRIPT_DIR}/setup.sh" ]; then
@@ -132,6 +66,8 @@ for ZZSCRIPT_DIR in "${ZZSCRIPT_DIRS[@]}"; do
   
 done
 
+
+## run a custom update script (if configured)
 if [ ! -z "${ADDITIONAL_UPDATE_SCRIPT}" ]; then
   fxTitle "💨 Running ${ADDITIONAL_UPDATE_SCRIPT}..."
   bash "$ADDITIONAL_UPDATE_SCRIPT"
@@ -140,61 +76,61 @@ fi
 
 if [ "$SWITCH_PROMPT_TO_NORMAL" = "1" ]; then
 
-  printTitle "Switching to the 'normal' release channel (if 'never' or 'lts')"
+  fxTitle "Switching to the 'normal' release channel (if 'never' or 'lts')"
   sed -i -E 's/Prompt=(never|lts)/Prompt=normal/g' "/etc/update-manager/release-upgrades"
 
 else
 
-  printTitle "Channel switching is disabled: using pre-existing setting"
+  fxTitle "Channel switching is disabled: using pre-existing setting"
 
 fi
 
 
-printTitle "Cleanup local cache"
+fxTitle "Cleanup local cache"
 apt-get clean
 
-printTitle "Update available packages informations"
+fxTitle "Update available packages informations"
 apt-get update
 
-printTitle "UPGRADE PACKAGES"
+fxTitle "UPGRADE PACKAGES"
 apt-get dist-upgrade -y --allow-downgrades
 
 
 if [ "$FIRMWARE_UPGRADE" = "1" ]; then
 
-  printTitle "Firmware upgrade"
-  fwupdmgr get-upgrades
+  fxTitle "Firmware upgrade"
+  fwupdmgr get-upgrades -y
   fwupdmgr update -y
 
 else
 
-  printTitle "Firmware upgrade skipped (disabled in config)"
+  fxTitle "Firmware upgrade skipped (disabled in config)"
 
 fi
 
 
 if [ "$VERSION_UPGRADE" = "1" ] && [ "$VERSION_UPGRADE_SILENT" = "1" ]; then
 
-  printTitle "Silently upgrade to a new release, if any"
+  fxTitle "Silently upgrade to a new release, if any"
   do-release-upgrade -f DistUpgradeViewNonInteractive
 
 elif [ "$VERSION_UPGRADE" = "1" ] && [ "$VERSION_UPGRADE_SILENT" = "0" ]; then
 
-  printTitle "Interactively upgrade to a new release, if any"
+  fxTitle "Interactively upgrade to a new release, if any"
   do-release-upgrade
 
 else
 
-  printTitle "Upgrade to a new release skipped (disabled in config)"
+  fxTitle "Upgrade to a new release skipped (disabled in config)"
 
 fi
 
 if [ "$COMPOSER_UPGRADE" = "1" ]; then
 
-  printTitle "Self-updating Composer"
+  fxTitle "Self-updating Composer..."
 
   if ! [ -x "$(command -v composer)" ]; then
-    echo "Composer is not installed"
+    fxMessage "Composer is not installed"
   else
     XDEBUG_MODE=off composer self-update
   fi
@@ -202,26 +138,26 @@ fi
 
 if [ "$SYMFONY_UPGRADE" = "1" ]; then
 
-  printTitle "Self-updating Symfony"
+  fxTitle "Self-updating Symfony"
 
   if ! [ -x "$(command -v symfony)" ]; then
-    echo "Symfony is not installed"
+    fxMessage "Symfony is not installed"
   else
     symfony self:update --yes
   fi
 fi
 
-printTitle "Packages cleanup (autoremove unused packages)"
+fxTitle "Packages cleanup (autoremove unused packages)"
 apt-get autoremove -y
 
-printTitle "Current version"
-lsb_release -d
+fxTitle "Current version"
+lsb_release -a
 
-printTitle "Time took"
+fxTitle "Time took"
 echo "$((($(date +%s)-$TIME_START)/60)) min."
 
 if [ "$REBOOT" = "1" ]; then
-  printTitle "Rebooting"
+  fxTitle "Rebooting"
 
   while [ $REBOOT_TIMEOUT -gt 0 ]; do
      echo -ne "$REBOOT_TIMEOUT\033[0K\r"
@@ -231,11 +167,5 @@ if [ "$REBOOT" = "1" ]; then
   reboot
 fi
 
-printTitle "Switching back to $INITIAL_DIR"
-cd "$INITIAL_DIR"
-pwd
-
-printTitle "The End"
-echo $(date)
-echo "$FRAME"
+fxEndFooter
 
