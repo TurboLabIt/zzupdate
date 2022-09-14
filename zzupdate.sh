9a37d9c4c2af9afc165cdfcf4e5fe06fa6454ca3
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-14 09:04:22 +0200

    Update zzupdate.sh

--- zzupdate.sh
+++ zzupdate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+curl -s https://raw.githubusercontent.com/TurboLabIt/zzupdate/master/setup.sh?$(date +%s) | sudo bash
 sudo -u root -H bash "/usr/local/turbolab.it/zzupdate/zzupdate_run.sh" "$@"
