2eeb13c8b1c8175233781d5a52bb87b952077308
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-17 21:11:43 +0100

    Update zzupdate_run.sh

--- zzupdate_run.sh
+++ zzupdate_run.sh
@@ -47,6 +47,7 @@ for ZZSCRIPT_DIR in "${ZZSCRIPT_DIRS[@]}"; do
   fi
 
   fxTitle "***** Update ${ZZSCRIPT_DIR}... *****"
+  git config --global --add safe.directory "$ZZSCRIPT_DIR"
   git -C "$ZZSCRIPT_DIR" reset --hard
   git -C "$ZZSCRIPT_DIR" pull --no-rebase
 
