d2f53ade88bca7228bb2b7e57215c9e25ed1ef94
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-17 21:34:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 echo ""
 SCRIPT_NAME=zzupdate
 
+## suppress needrestart prompts
+if [ -e "/etc/needrestart/needrestart.conf" ]; then
+
+  # https://askubuntu.com/a/1424249/181869
+  sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
+  
+  # https://askubuntu.com/a/1421221/181869
+  sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
+fi
+
 ## bash-fx
 if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
 curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/master/setup.sh?$(date +%s) | sudo bash
